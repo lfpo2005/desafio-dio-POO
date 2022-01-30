882d95dcd3e138f596795a10adea5e76b9153f38
Fernando Oliveira <47955596+lfpo2005@users.noreply.github.com>
2022-01-29 22:13:09 -0300

    concluindo a criacao das classes do dominio

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,35 +1,51 @@
 package br.com.dio.desafio.dominio;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 @Data
 public class Dev {
 
     private String nome;
-    private Set<Conteudo> conteudoInscrito = new LinkedHashSet<>();
-    private Set<Conteudo> conteudoComcluidos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp() {}
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
 
-    public void progresso() {}
+    public void progresso() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudoConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else{
+            System.err.println("Você não esta matriculado em nem um conteudo!");
+        }
+    }
 
-    public void calcularTotalXP() {}
+    public double calcularTotalXP() {
+       return this.conteudoConcluidos
+               .stream()
+               .mapToDouble(Conteudo::calcularXP)
+               .sum();
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Dev dev = (Dev) o;
-        return Objects.equals(nome, dev.nome) && Objects.equals(conteudoInscrito, dev.conteudoInscrito) && Objects.equals(conteudoComcluidos, dev.conteudoComcluidos);
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudoConcluidos, dev.conteudoConcluidos);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nome, conteudoInscrito, conteudoComcluidos);
+        return Objects.hash(nome, conteudosInscritos, conteudoConcluidos);
     }
 }
